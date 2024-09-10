675b128e124a58ac14d7557921d022d5825d3196
lmaletic <luk.male@gmail.com>
2024-09-10 09:48:48 +0200

    first commit

---
+++ src/main/java/hr/tis/hackaton/sightseeingapp/SightSeeingAppApplication.java
@@ -0,0 +1,13 @@
+package hr.tis.hackaton.sightseeingapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SightSeeingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SightSeeingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hr/tis/hackaton/sightseeingapp/SightSeeingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package hr.tis.hackaton.sightseeingapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SightSeeingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
